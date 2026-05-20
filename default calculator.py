e721314a8782c656d18e1572c4a408d4c858d0b9
zibatop <zibanika541@gmail.com>
2026-05-20 19:34:27 +0300

    test

--- default calculator.py
+++ default calculator.py
@@ -1,9 +1,21 @@
-a = float(input("введите первое число: "))
-b = float(input("введите второе число: "))
-print("сумма: ", a + b)
-print("разность: ", a - b)
-print("умножение: ", a * b)
-if b > 0:
-    print("деление: ", a / b)
-else:
-    print("на 0 делить нельзя!")
+import tkinter as tk
+def calculate():
+    try:
+        result = eval(entry.get())
+        result_label.config(text=f"Ответ: {result}")
+    except:
+        result_label.config(text="Ошибка")
+window = tk.Tk()
+window.title("Калькулятор")
+window.geometry("500x300")
+
+entry = tk.Entry(window, font=("Arial", 16))
+entry.pack(pady=20)
+
+button = tk.Button(window, text="Посчитать", command=calculate)
+button.pack()
+
+result_label = tk.Label(window, text="", font=("Arial", 14))
+result_label.pack(pady=20)
+
+window.mainloop()
